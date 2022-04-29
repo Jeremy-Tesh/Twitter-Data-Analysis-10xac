b38005ca97a90e837d7bfc19245e7c85cc9275d6
Jeremy Tesh <jermitesh20@gmail.com>
2022-04-29 17:54:33 +0300

    feat:strelimt sidebar and data insertion

--- streamlit python/analysis.py
+++ streamlit python/analysis.py
@@ -1,3 +1,4 @@
+from select import select
 import streamlit as st
 import pandas as pd
 import matplotlib as plt
@@ -5,7 +6,7 @@ import matplotlib as plt
 import numpy as np
 
 
-st.title('Tweet Sentinment Analysis')
+st.title('Twitter Data Analysis')
 
 st.sidebar.title("Tweetiter Analysis")
 st.sidebar.header("heyy")
@@ -16,6 +17,22 @@ data=pd.read_csv('../cleaned_tweet_data.csv')
 if st.checkbox("Show Data"):
     st.write(data.head(50))
 
+select=st.sidebar.selectbox('Models',['Topic Model','Sentiment Analysis'])
 
+if select == "Sentiment Analysis":
 
-st.sidebar.subheader('Tweet Analyser')
+    st.sidebar.subheader('Tweet Analyser')
+    tweets=st.sidebar.radio('Sentiment Type',('positive','negative','neutral'))
+    polarity=st.write(data.query('polarity==0')[['original_text']].sample(1).iat[0,0])
+
+    visualizers=st.sidebar.radio('Visualization of tweets',['Bar Chart','Pie Chart'])
+
+    if tweets == "positive":
+        polarity=st.write(data.query('polarity>0')[['original_text']].head(5))
+    elif tweets == "negative":
+        polarity=st.write(data.query('polarity<0')[['original_text']].head(5))
+    else:
+        polarity=st.write(data.query('polarity==0')[['original_text']].head(5))
+else:
+
+    st.write("no data")
